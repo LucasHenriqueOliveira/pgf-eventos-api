62c78a4127f4b6d71d3a0888f753198c38055ee5
Lucas <lucashen@gmail.com>
2018-10-26 08:32:39 -0300

    add new routes

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,20 @@ Route::group([
 ], function ($router) {
 
     Route::post('crawler', 'CrawlerController@process');
+
     Route::get('marcas', 'MarcasController@getMarcas');
     Route::get('marcas/{id}', 'MarcasController@getModelos');
+
     Route::post('manual', 'ManualController@save');
     Route::get('manual', 'ManualController@get');
+
+    Route::get('status', 'StatusController@get');
+    Route::post('status', 'StatusController@save');
+
+    Route::get('uso', 'UsoController@get');
+    Route::post('uso', 'UsoController@save');
+    Route::delete('uso/{id}', 'UsoController@remove');
+
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
     Route::post('logout', 'AuthController@logout');
